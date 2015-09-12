117696a9a49c87ed7a8d223d862e35237265bc87
Peter Borshchahivskyi <peb@ciklum.com>
2015-09-12 15:44:53 +0300

    Fix output

--- index.js
+++ index.js
@@ -10,9 +10,12 @@
 var extend = require('util')._extend;
 var chalk = require('chalk');
 
-var currentFile4Log = null;
+var currentFile4Log = {};
 var config = {
-    silence: false
+    silence: false,
+    filename: false,
+    grepper: null,
+    space: '  '
 };
 
 function log() {
@@ -21,17 +24,22 @@ function log() {
     try { throw new Error() } catch(e) {
         var stackLines = e.stack.split('\n');
         var pos = stackLines[2].match(/\((.+?):(\d+):(\d+)\)/); // file:string:column
-        if (!currentFile4Log) currentFile4Log = require('fs').readFileSync(pos[1]).toString().split(/\n/);
-        params = currentFile4Log[pos[2] - 1].substr(pos[3] - 1).match(/.+?\((.*?)\)/)[1].split(/\s*,\s*/);
+        if (!currentFile4Log[pos[1]]) currentFile4Log[pos[1]] = require('fs').readFileSync(pos[1]).toString().split(/\n/);
+        params = currentFile4Log[pos[1]][pos[2] - 1].substr(pos[3] - 1).match(/.+?\((.*?)\)/)[1].split(/\s*,\s*/);
     }
 
     params = params.reduce(function(result, param, i) {
+        if (i >= arg.length) return result;
         return /^['"]/.test(param) || /^[\d.]+$/.test(param) || /^[{\[]/.test(param)
-            ? result.concat(chalk.blue(param))
-            : result.concat(chalk.blue(param) + ' = ' + chalk.yellow(JSON.stringify(arg[i])));
+            ? result.concat(chalk.blue( typeof arg[i] === 'object' ? JSON.stringify(arg[i], null, config.space) : arg[i] ))
+            : result.concat(chalk.blue(param) + ' = ' + chalk.yellow(JSON.stringify(arg[i], null, config.space)));
     }, []);
 
-    if (!config.silence) console.log(params.join(', '));
+    var result = [params.join(', ')];
+    if (config.filename) result.unshift(chalk.gray('[' + pos[1].replace(/^.+[/\\]([^/\\]+)$/, '$1') + ':' + pos[2] + ']'));
+    if (config.grepper) result.unshift(config.grepper);
+
+    if (!config.silence) console.log.apply(console, result);
 }
 
 module.exports = function(settings) {
